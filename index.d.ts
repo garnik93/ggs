6a1f251b6e2afb96ae4a9c4f6eaa30b2935f69ee
emojie <gepard4125@gmail.com>
2024-01-17 15:43:08 +0200

    update

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,36 @@
-import  ConfigService  from './lib/config.service'
+import ConfigService from './lib/config.service'
 
-declare module 'ggs-database' {
-    type DatabaseClientOptions = {
-        store: {
-            username: string
-            password: string
-            projectName: string
-            secreteKey?: string
-            host?: string
-            port?: number
-        }
+type DatabaseClientOptions = {
+    store: {
+        username: string
+        password: string
+        projectName: string
+        secreteKey?: string
+        host?: string
+        port?: number
     }
+}
 
-    class GGSClient {
-        private config: ConfigService
-        private checkUsername: string
-        private checkPassword: string
-        private username: string
-        private password: string
-        private projectName: string
-        private secreteKey: string
-        private host: string
-        private port: number
+export default class GGSClient {
+    private config: ConfigService
+    private checkUsername: string
+    private checkPassword: string
+    private username: string
+    private password: string
+    private projectName: string
+    private secreteKey: string
+    private host: string
+    private port: number
 
-        constructor(options: DatabaseClientOptions)
-        private authenticate(): Promise<string | null>
-        set(collection: string, item: any): Promise<void>
-        get(collection: string): Promise<any>
-        verifyToken(secretKey: string, token: string): Promise<any | null>
-        generateToken(data: string, secretKey: string): string
-    }
+    constructor(options: DatabaseClientOptions)
 
-    export = GGSClient
+    private authenticate(): Promise<string | null>
+
+    set(collection: string, item: any): Promise<void>
+
+    get(collection: string): Promise<any>
+
+    verifyToken(secretKey: string, token: string): Promise<any | null>
+
+    generateToken(data: string, secretKey: string): string
 }
