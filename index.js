a428aa244cc8300f7a994ee19c02bad66d2dcdc6
emojie <gepard4125@gmail.com>
2024-02-06 11:50:08 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken')
 const ConfigService = require('./lib/config.service')
-const axios = require("axios");
+const axios = require("axios")
 
 class GGSClient {
+    axiosConfig = (token) => ({headers: {'Authorization': `Bearer ${token}`}})
+
     constructor(options) {
         this.config = new ConfigService()
         this.checkUsername = process.env.GGS_USERNAME
@@ -10,16 +12,17 @@ class GGSClient {
         this.username = options.store.username
         this.password = options.store.password
         this.projectName = options.store.projectName
-        this.secreteKey = options.store.secreteKey || this.config.get('SECRETE_KEY')
-        this.host = options.store.host || process.env.GGS_HOST
-        this.port = options.store.port || 3000
+        this.secreteKey = options.store.secreteKey
+        this.host = options.store.host
+        this.port = options.store.port
+        this.url = (id) => `http://${this.host}:${this.port}/api/${this.projectName}/${id}`
     }
 
     async authenticate() {
         const {username, password} = this
 
         if (username !== this.checkUsername || password !== this.checkPassword) return null
-        return this.generateToken({ username: this.username }, this.secreteKey)
+        return this.generateToken({username: this.username}, this.secreteKey)
     }
 
     async set(collection, item) {
@@ -31,13 +34,7 @@ class GGSClient {
 
         try {
             const id = collection.match(/\d+/) ? `${collection}:${collection}` : collection
-            await axios.post(`http://${this.host}:${this.port}/api/${this.projectName}/${id}`, JSON.stringify(item),{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            })
+            await axios.post(this.url(id), JSON.stringify(item), this.axiosConfig(token))
         } catch (error) {
             console.error(error)
             throw new Error('Failed to add item')
@@ -53,13 +50,7 @@ class GGSClient {
 
         try {
             const id = collection.match(/\d+/) ? `${collection}:${collection}` : collection
-            const response = await axios.get(`http://${this.host}:${this.port}/api/${this.projectName}/${id}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            })
+            const response = await axios.get(this.url(id), this.axiosConfig(token))
 
             const items = await response.data
             return items || {}
